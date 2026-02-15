48f3fc79e61a8a99916caa8a98bd8c1c85145288
Asad777 <muhammadasad7.7.2007@gmail.com>
2026-02-15 15:15:45 +0500

    fixed sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -8,17 +8,17 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 1,
     },
-    {
-      url: `${baseUrl}/projects`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
+    // {
+    //   url: `${baseUrl}/projects`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: `${baseUrl}/contact`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.7,
+    // },
   ];
 }
