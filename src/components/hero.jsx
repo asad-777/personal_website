84d1f33f5891aa3af3536cdf308bb3a97464d416
Asad777 <muhammadasad7.7.2007@gmail.com>
2026-04-24 19:46:38 +0500

    old glass idea cancle, new idea start here

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,15 +1,12 @@
-"use client"
-import { useEffect, useRef, useState } from "react";
-import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
-import GradientText from './GradientText';
-
+'use client'
 
 export default function Hero() {
-
-
-    return (
-        <div  id="hero" className="relative w-full h-screen overflow-hidden font-sans bg-base-100">
-            asad
-        </div>
-    )
+  return (
+    <section
+      id="hero"
+      className="relative w-full h-screen overflow-hidden bg-base-100 flex items-center justify-center"
+    >
+      {/* Hero content goes here */}
+    </section>
+  )
 }
