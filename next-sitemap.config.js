423a9a12e8bd856e64f19f909d28e794cb5b3cd2
Asad777 <muhammadasad7.7.2007@gmail.com>
2026-04-27 18:37:05 +0500

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://asadamir.vip', // MUST be your real domain
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+}
